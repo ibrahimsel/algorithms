a4d6953068d5f4e10b7553e1ff7ee303d6477529
Ibrahim <63649222+ibrahimsel@users.noreply.github.com>
2020-10-13 00:01:45 +0300

    Update SortingAlgorithms.cpp

--- SortingAlgorithms.cpp
+++ SortingAlgorithms.cpp
@@ -1,6 +1,5 @@
 // THIS CONSOLE APP IS NOT DONE YET
 
-
 #include "SortingAlgorithms.h"
 #include "UsefulFuncs.h"
 #include <windows.h>
@@ -8,7 +7,7 @@
 #include <chrono>
 #include <iomanip>
 
-UsefulFuncs useless;
+UsefulFuncs useful;
 
 void SortingAlgorithms::bubbleSort(int arr[], int size)
 {
@@ -19,7 +18,7 @@ void SortingAlgorithms::bubbleSort(int arr[], int size)
         for (int i = 0; i < size - 1; i++)
         {
             if (arr[i] > arr[i + 1])
-                useless.swap(&arr[i], &arr[i + 1]);
+                useful.swap(&arr[i], &arr[i + 1]);
         }
         c++;
     }
@@ -29,18 +28,47 @@ void SortingAlgorithms::bubbleSort(int arr[], int size)
     std::cout << "\n\n\nBubble Sorted array: ";
     for (int i = 0; i < size; i++)
         std::cout << std::setw(4) << arr[i];
-    std::cout << "\n\n------------------> " << "Elapsed time during Bubble Sort: " << elapsed << " microseconds\n";
-
+    std::cout << "\n\n------------------> "
+              << "Elapsed time during Bubble Sort: " << elapsed << " microseconds\n";
 }
 
-/*
 void SortingAlgorithms::selectionSort(int arr[], int size)
 {
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < size; i++)
-    {       
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        useful.swap(&arr[i], &arr[minIndex]);
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "\n\nSelection Sorted array: ";
+    for (int i = 0; i < size; i++)
+        std::cout << std::setw(4) << arr[i];
+    std::cout << "\n\n------------------> "
+              << "Elapsed time during Selection Sort: " << elapsed << " microseconds\n";
 }
-*/
+
+/* void SortingAlgorithms::insertionSort(int arr[], int size)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    int toSwap;
+    while ()
+    {
+        // [5,3,1,6,7,8,2]
+
+        int i = 1;
+        if (arr[i] < arr[i - 1])
+        {
+            toSwap = arr[i--];
+
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+} */
