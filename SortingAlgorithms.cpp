7d04f0256838a2ece41a63c86926904c536af38e
Ibrahim <63649222+ibrahimsel@users.noreply.github.com>
2020-10-15 21:48:05 +0300

    Update SortingAlgorithms.cpp

--- SortingAlgorithms.cpp
+++ SortingAlgorithms.cpp
@@ -54,21 +54,26 @@ void SortingAlgorithms::selectionSort(int arr[], int size)
               << "Elapsed time during Selection Sort: " << elapsed << " microseconds\n";
 }
 
-/* void SortingAlgorithms::insertionSort(int arr[], int size)
+void SortingAlgorithms::insertionSort(int arr[], int size)
 {
     auto start = std::chrono::high_resolution_clock::now();
-    int toSwap;
-    while ()
+    int i, j, compare;
+    for (int i = 1; i < size; i++)
     {
-        // [5,3,1,6,7,8,2]
-
-        int i = 1;
-        if (arr[i] < arr[i - 1])
+        compare = arr[i];
+        j = i - 1;
+        while (arr[j] > compare && j >= 0)
         {
-            toSwap = arr[i--];
-
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = compare;
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-} */
+    std::cout << "\nInsertion Sorted array: ";
+    for (int i = 0; i < size; i++)
+        std::cout << std::setw(4) << arr[i];
+    std::cout << "\n\n------------------> "
+              << "Elapsed time during Insertion Sort: " << elapsed << " microseconds\n";
+}
